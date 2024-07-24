64b4dcb4af74e6084c807f3ba59f28129a051377
Robert <9291792+bushshrub@users.noreply.github.com>
2024-07-24 10:02:05 -0400

    update gh page generation script

--- generate_gh_page.py
+++ generate_gh_page.py
@@ -8,7 +8,34 @@ if __name__ == "__main__":
     
     with open("gh-pages/index.html", "w") as f:
         f.write("<!DOCTYPE html>\n")
-        f.write("<html>\n<body>\n")
+        f.write('''
+                <html>
+                <head>
+                <style>
+                
+        body {
+            font-size: 20pt;
+        }
+        @media (prefers-color-scheme: dark) {
+            body {
+                background-color: black;
+                color: #fdfdfd;
+            }
+            a {
+                color: #ffe666;
+                text-decoration: none;
+            }
+            a:hover {
+                background: #424242;
+                color: #fdfdfd;
+                text-decoration: underline;
+            }
+        }
+
+                </style>
+                </head>
+                ''')
+        f.write("<body>\n")
         f.write("<h1>Algebra Book Build Files</h1>\n")
         current_git_commit_hash = os.popen("git rev-parse --short HEAD").read().strip()
         last_commit_dt = os.popen('git log -1 --date=format:"%Y/%m/%d %T" --format="%ad"').read().strip()
